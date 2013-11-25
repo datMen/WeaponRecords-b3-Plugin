5f8f3aab3ae259176e39c77955c44b4d10fe544c
LouK <8thelouk8@gmail.com>
2013-11-25 15:52:32 +0100

    Lets use findWeapon on !weaponstats

--- weaponrecord.py
+++ weaponrecord.py
@@ -136,12 +136,12 @@ class WeaponrecordPlugin(b3.plugin.Plugin):
             return False
         
         input = self._adminPlugin.parseUserCmd(data)
-        weapon = input[0]
+        weapon = self.findweapon(input[0])
         cname = input[1]
         if cname:
             sclient = self._adminPlugin.findClientPrompt(cname, client)
-            stats = self.console.storage.query('SELECT %s FROM weaponstats WHERE client_id = "%s"' % (weapon, sclient.id))
-            cmd.sayLoudOrPM(client, '^2%s ^7Kills: %s ^7: %s' % (weapon, sclient.exactName, stats))
+            stats = self.console.storage.query('SELECT %s FROM weaponstats WHERE client_id = "%s"' % (weapon[1], sclient.id))
+            cmd.sayLoudOrPM(client, '^2%s ^7Kills: %s ^7: %s' % (weapon[0], sclient.exactName, stats))
         else:
-            stats = self.console.storage.query('SELECT %s FROM weaponstats WHERE client_id = "%s"' % (weapon, client.id))
-            cmd.sayLoudOrPM(client, '^2%s ^7Kills: %s ^7: %s' % (weapon, client.exactName, stats))
+            stats = self.console.storage.query('SELECT %s FROM weaponstats WHERE client_id = "%s"' % (weapon[1], client.id))
+            cmd.sayLoudOrPM(client, '^2%s ^7Kills: %s ^7: %s' % (weapon[0], client.exactName, stats))
