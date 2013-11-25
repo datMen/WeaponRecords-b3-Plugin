e48a6266edf60d1ec69ad0c825c6b72c821ec393
TheLouK <8thelouk8@gmail.com>
2013-11-25 12:37:27 +0100

    Added config file and first commands
    Config file allows administrators select the required level for each
    command easily.

--- weaponrecord.py
+++ weaponrecord.py
@@ -6,10 +6,24 @@ import b3
 import b3.plugin
 
 class WeaponrecordPlugin(b3.plugin.Plugin):
-    requiresConfigFile = False
  
     def onLoadConfig(self):
         self._adminPlugin = self.console.getPlugin('admin')
         if not self._adminPlugin:
             self.error('Could not find admin plugin')
             return False
+
+        # Register commands
+        if 'commands' in self.config.sections():
+            for cmd in self.config.options('commands'):
+                level = self.config.get('commands', cmd)
+                sp = cmd.split('-')
+                alias = None
+                # Alias for the command
+                if len(sp) == 2:
+                    cmd, alias = sp
+
+                # Find function for this command
+                func = self.getCmd(cmd)
+                if func:
+                    self._adminPlugin.registerCommand(self, cmd, level, func, alias)
